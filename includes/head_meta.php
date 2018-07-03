8a276ee002804c87cf0d64edbb47b57cd67bcbc0
Giulio Gratta <giuliog@stanford.edu>
2018-07-02 19:44:30 -0700

    Add Font Awesome

--- includes/head_meta.php
+++ includes/head_meta.php
@@ -4,6 +4,9 @@
 <meta name="description" content="Website explaining the use of the Stop Wage Theft API">
 
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Source+Code_Pro:600" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/solid.css" integrity="sha384-TbilV5Lbhlwdyc4RuIV/JhD8NR+BfMrvz4BL5QFa2we1hQu6wvREr3v6XSRfCTRp" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/brands.css" integrity="sha384-7xAnn7Zm3QC1jFjVc1A6v/toepoG3JXboQYzbM0jrPzou9OFXm/fY6Z/XiIebl/k" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/fontawesome.css" integrity="sha384-ozJwkrqb90Oa3ZNb+yKFW2lToAWYdTiF1vt8JiH5ptTGHTGcN7qdoR1F95e0kYyG" crossorigin="anonymous">
 
 <style>
     body {
