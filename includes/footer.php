655079233761b3a0851fdec2ba51be2c0f15d8dd
Giulio Gratta <giuliog@stanford.edu>
2018-07-02 19:44:52 -0700

    Add "get involved" links to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
 <footer id="footer">
-    <p>&copy; <?php echo date("Y"); ?>, all rights reserved.</p>
+    <p class="top">
+        Get Involved: 
+        <a href="https://github.com/wagetheft"><i class="fab fa-github"></i> Github</a>
+        <a href="wagetheft.slack.com"><i class="fas fa-comments"></i> Slack</a>
+        <a href="https://trello.com/stopwagetheft"><i class="fas fa-tasks"></i> Trello</a>
+    </p>
+    <p class="top copyright">&copy; <?php echo date("Y"); ?>, all rights reserved.</p>
 </footer>
