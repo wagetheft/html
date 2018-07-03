dd5f7929717307950bad6eea2cc6ad255a352703
Giulio Gratta <giuliog@stanford.edu>
2018-07-02 19:48:07 -0700

    Fix Slack link

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <p class="top">
         Get Involved: 
         <a href="https://github.com/wagetheft"><i class="fab fa-github"></i> Github</a>
-        <a href="wagetheft.slack.com"><i class="fas fa-comments"></i> Slack</a>
+        <a href="https://wagetheft.slack.com"><i class="fas fa-comments"></i> Slack</a>
         <a href="https://trello.com/stopwagetheft"><i class="fas fa-tasks"></i> Trello</a>
     </p>
     <p class="top copyright">&copy; <?php echo date("Y"); ?>, all rights reserved.</p>
